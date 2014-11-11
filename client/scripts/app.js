9ee8d8276654de8706ba6af68fa25174050c2281
renderf0x <derek@barncard.com>
2014-11-11 12:33:42 -0800

    basic functionality complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,83 +1,84 @@
 /* GLOBALS *************************************/
-
+/* rooms and friends */
 var roomNames = {
   home: true
 };
 
-var messageTemplate;
+var friends = {};
 
+/* main Handlebars templates - compiled below*/
+var messageTemplate;
+var friendTemplate;
+var friendMessageTemplate;
+
+/* setup click and event handlers, and compile templates*/
+/* in document.ready so it will wait for page load*/
 $(document).ready(function(){
+
+  /*post buttom*/
   $(".sendPost").click(function(){
   postMessage();
-});
+  });
 
+  /*send on enter*/
+  $('input').keypress(function (e) {
+      if (e.which == '13') {
+        postMessage();
+      }
+  });
+
+  /*create room [+] button*/
+  $('.createRoom').click(function(){
+    createRoom();
+  })
+
+  /*initial call to fetch messages*/
   fetchMessages();
 
+  /*handlebars template compilation*/
   var source   = $(".messageTemplate").html();
   messageTemplate = Handlebars.compile(source);
 
+  var friendMessageSrc = $(".friendMessageTemplate").html();
+  friendMessageTemplate = Handlebars.compile(friendMessageSrc);
+
+  var friendsSource = $("#friendTemplate").html();
+  friendTemplate = Handlebars.compile(friendsSource);
+
+  /* set recurring fetch time*/
+  setInterval(fetchMessages, 3000);
+
 });
 
-var fetchMessages = function(){
-  var params = '';
-  console.log(JSON.stringify(roomNames));
-  if(!roomNames['home']){
-    for(key in roomNames){
-      if(roomNames[key]){
-        params = 'where={"roomname":"'+key+'"}';
-      }
-    }
-  }
-  console.log(params);
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    data: 'order=-createdAt;limit=20;' + params,
-    contentType: 'application/json',
-    success: function (data) {
-      displayMessages(data.results);
-      displayRooms();
 
-      //console.log(JSON.stringify(data.results));
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to load messages');
-    }
-  });
+
+/* FRIENDS ***********************************/
+/* adds to global object friends, where name is key and*/
+/* initial state is false*/
+
+var addFriend = function(name){
+  friends[name] = true;
+  displayFriendsList();
 }
 
-setInterval(fetchMessages, 3000);
+var displayFriendsList = function(){
+  $('.friendslist').empty();
+  var friendsArray = Object.keys(friends);
+  $('.friendslist').append(friendTemplate(friendsArray));
+}
 
-var postMessage = function(){
-  var postContent = $("input").val();
-  var message = {
-  'username': 'peter',
-  'text': postContent,
-  'roomname': $('li.active').text()
-  };
-
-
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('success!')
-    fetchMessages();
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to load messages');
-    }
-  });
-};
-
+var setUserLinks = function(){
+  $('.userLink').click(function(){
+      var name = $(this).text();
+      console.log('FRIENDDDDD: ' + $(this).text());
+      addFriend(name);
+    })
+}
 
 
 /* ROOMS *************************************/
+/* adds to global object roomNames, where name is key and*/
+/* initial state is false*/
 
 var populateRooms = function(message){
   if(message.roomname && roomNames[message.roomname] === undefined && sanityCheck(message.roomname)){
@@ -126,7 +127,45 @@ var setActiveRoom = function(room){
   //$('#'+room).addClass('active');
 }
 
+var createRoom = function(){
+  var newRoom = prompt("Please choose a room name");
+  roomNames[newRoom] = false;
+  displayRooms();
+  setActiveRoom(newRoom);
+}
+
 /* MESSAGES *********************************/
+
+var fetchMessages = function(){
+  var params = '';
+  console.log(JSON.stringify(roomNames));
+  if(!roomNames['home']){
+    for(key in roomNames){
+      if(roomNames[key]){
+        params = 'where={"roomname":"'+key+'"}';
+      }
+    }
+  }
+  console.log(params);
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: 'order=-createdAt;limit=20;' + params,
+    contentType: 'application/json',
+    success: function (data) {
+      displayMessages(data.results);
+      displayRooms();
+      setUserLinks();
+
+      //console.log(JSON.stringify(data.results));
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to load messages');
+    }
+  });
+}
 
 var displayMessages = function(messages){
   $(".messages").empty();
@@ -136,19 +175,53 @@ var displayMessages = function(messages){
   });
 }
 
+var displayMessage = function(message){
+  if(message.username && sanityCheck(message.text) && sanityCheck(message.username)){
+    //console.log("Username string: " + message.username + ", friend:" + friends[message.username]);
+    if(friends[message.username] !== undefined){
+      //console.log("calling friend-specific template");
+      $(".messages").append(friendMessageTemplate(message));
+    }else{
+      $(".messages").append(messageTemplate(message));
+    //console.log("Message Text: " + message.text);
+    }
+  }
+}
+
+var postMessage = function(){
+  var postContent = $("input").val();
+  var message = {
+  'username': 'peter',
+  'text': postContent,
+  'roomname': $('li.active').text()
+  };
+
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('success!')
+    fetchMessages();
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to load messages');
+    }
+  });
+
+  $('input').val("");
+};
+/* MESSAGE HELPERS **************************/
+
 var escapeText = function(text){
   return validator.escape(text);
 }
 
 var sanityCheck = function(text){
-  var regObj = new RegExp(/[\W]/i);
-  return !regObj.test(text);
+  var regObj = new RegExp(/[\w\s]/i);
+  return regObj.test(text);
 };
-
-var displayMessage = function(message){
-  if(message.username && sanityCheck(message.text) && sanityCheck(message.username)){
-    $(".messages").append(messageTemplate(message));
-    //console.log("Message Text: " + message.text);
-  }
-}
 
