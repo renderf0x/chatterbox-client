ebf7bee63a7f429a42186188135bd97d2803e8aa
renderf0x <derek@barncard.com>
2014-11-10 19:58:20 -0800

    Filtering by room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,29 @@
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  //data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    displayMessages(data.results);
-    displayRooms();
-    //console.log(JSON.stringify(data));
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to load messages');
+var fetchMessages = function(){
+  var params = '';
+  if(!roomNames['home']){
+    params = 'where={"roomname":"peter"}';
   }
-});
+
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: 'order=-createdAt;limit=20' + params,
+    contentType: 'application/json',
+    success: function (data) {
+      displayMessages(data.results);
+      displayRooms();
+
+      //console.log(JSON.stringify(data.results));
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to load messages');
+    }
+  });
+}
+
+setInterval(fetchMessages, 3000);
 
 var postMessage = function(){
   var postContent = $("input").val();
@@ -30,7 +40,8 @@ var postMessage = function(){
   data: JSON.stringify(message),
   contentType: 'application/json',
   success: function (data) {
-    console.log('success!');
+    console.log('success!')
+    fetchMessages();
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -40,25 +51,48 @@ var postMessage = function(){
 };
 
 
-var roomNames = {};
+var roomNames = {
+  home: true
+};
 
 /* ROOMS *************************************/
 
 var populateRooms = function(message){
-  if(message.roomname && roomNames[message.roomname] === undefined){
-    roomNames[message.roomname] = message.roomname;
+  if(message.roomname && roomNames[message.roomname] === undefined && sanityCheck(message.roomname)){
+    roomNames[message.roomname] = false;
+    // console.log(JSON.stringify(roomNames));
   }
 }
 
 var displayRooms = function(){
+  $('.rooms').empty();
   for (key in roomNames){
-    $('.rooms').append('<li role="presentation" class="roomName"><a href="#">' + roomNames[key] + '</a></div>');
+    $('.rooms').append('<li role="presentation" class="roomName" id="' + key + '"><a href="#">' + key + '</a></div>');
   }
+  $('.roomName').click(function(){
+    console.log('meow');
+    setActiveRoom($(this).text());
+  });
+}
+
+var setActiveRoom = function(room){
+  $('li.active').toggleClass('active');
+  for (key in roomNames){
+    if(key === room){
+      // console.log("setting " + key + "to true");
+      roomNames[key] = true;
+    }else{
+      // console.log("setting " + key + "to false");
+      roomNames[key] = false;
+    }
+  }
+  $('#'+room).toggleClass('active');
 }
 
 /* MESSAGES *********************************/
 
 var displayMessages = function(messages){
+  $(".messages").empty();
   _.each(messages, function(message){
     displayMessage(message);
     populateRooms(message);
@@ -69,14 +103,22 @@ var escapeText = function(text){
   return validator.escape(text);
 }
 
+var sanityCheck = function(text){
+  var regObj = new RegExp(/[\W]/i);
+  return !regObj.test(text);
+};
+
 var displayMessage = function(message){
-  //if(message.username){
-    $(".messages").append('<div class="message">'+escapeText(message.username) + " : " + escapeText(message.text) + '</div>' );
-  //}
+  if(message.username && sanityCheck(message.text) && sanityCheck(message.username)){
+    $(".messages").append('<div class="message">'+escapeText(message.username) + " : " + escapeText(message.text) + '-' + message.roomname + '</div>' );
+    //console.log("Message Text: " + message.text);
+  }
 }
 
 $(document).ready(function(){
   $(".sendPost").click(function(){
-  console.log('foo');
-  postMessage()});
-})
+  postMessage();
+});
+
+  fetchMessages();
+});
