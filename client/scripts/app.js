6ee897aef2561740549166bb8ce62ed86a30e116
renderf0x <derek@barncard.com>
2014-11-10 22:27:23 -0800

    stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 var fetchMessages = function(){
   var params = '';
+  console.log(JSON.stringify(roomNames));
   if(!roomNames['home']){
     for(key in roomNames){
       if(roomNames[key]){
@@ -34,7 +35,7 @@ var postMessage = function(){
   var message = {
   'username': 'peter',
   'text': postContent,
-  'roomname': 'peterRoom'
+  'roomname': $('li.active').text()
   };
 
 
@@ -67,31 +68,45 @@ var populateRooms = function(message){
     // console.log(JSON.stringify(roomNames));
   }
 }
-
+//$('.active').roomname === key
 var displayRooms = function(){
+  var activeRoom = $('.active').attr('id')
+  console.log('ACTIVE ROOM: ' + activeRoom);
   $('.rooms').empty();
   for (key in roomNames){
     $('.rooms').append('<li role="presentation" class="roomName" id="' + key + '"><a href="#">' + key + '</a></div>');
+    if(key === activeRoom){
+      $('#' + activeRoom).addClass('active');
+    }
   }
+
   $('.roomName').click(function(){
-    console.log('meow');
+    //console.log('meow');
     setActiveRoom($(this).text());
     fetchMessages();
   });
 }
 
 var setActiveRoom = function(room){
-  $('li.active').toggleClass('active');
+  //$('li.active').removeClass('active');
+  if(room === undefined){
+
+  }
   for (key in roomNames){
     if(key === room){
-      // console.log("setting " + key + "to true");
+      //console.log("setting " + key + "to true");
       roomNames[key] = true;
+      var id = '#'+room;
+      $(id).addClass('active');
     }else{
-      // console.log("setting " + key + "to false");
+      //console.log("setting " + key + "to false");
       roomNames[key] = false;
+      var id = '#'+key;
+      console.log($(id));
+      $(id).removeClass('active');
     }
   }
-  $('#'+room).toggleClass('active');
+  //$('#'+room).addClass('active');
 }
 
 /* MESSAGES *********************************/
@@ -112,10 +127,14 @@ var sanityCheck = function(text){
   var regObj = new RegExp(/[\W]/i);
   return !regObj.test(text);
 };
+//debugger;
+console.log
+var source   = $(".messageTemplate").html();
+var messageTemplate = Handlebars.compile(source);
 
 var displayMessage = function(message){
   if(message.username && sanityCheck(message.text) && sanityCheck(message.username)){
-    $(".messages").append('<div class="message">'+escapeText(message.username) + " : " + escapeText(message.text) + '-' + message.roomname + '</div>' );
+    $(".messages").append(messageTemplate(message));
     //console.log("Message Text: " + message.text);
   }
 }
